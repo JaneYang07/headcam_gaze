1e4fa67314271f46da5965e31e7d5129788fa4a3
Jane Yang <j7yang@ucsd.edu>
2022-11-28 14:21:20 -0600

    updated vis

--- stream_visualization.m
+++ stream_visualization.m
@@ -21,7 +21,7 @@ obj_num = 24;
 subexpIDs = find_subjects({'cont_vision_size_obj9_child'},[exp_id]);
 vars = {'cstream_inhand_left-hand_obj-all_child','cstream_inhand_right-hand_obj-all_child','cstream_vision_size_obj-largest-child-inhand','cstream_vision_size_obj-largest-dominant_child', 'cstream_eye-vision_largest-roi_child', 'cstream_eye_roi_child','cstream_vision_size_obj-largest-parent-inhand','cstream_inhand_right-hand_obj-all_parent','cstream_inhand_left-hand_obj-all_parent'};
 labels = {'c_roi','c_left', 'c_right','c_large','dominant','roi_large','c_roi','p_large','p_right','p_left'};
-args.window_times_variable = [0 200;200 400;400 600;];
+args.window_times_variable = [0 150;150 300;300 450;450 600];
 directory = '.';
 % note that directory = '.' will save in the current directory
 vis_streams_multiwork(subexpIDs, vars, labels, directory, args);
