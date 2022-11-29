caa5b2ea9631c9e5f08788364932261ce9b65005
Jane Yang <j7yang@ucsd.edu>
2022-11-29 12:40:01 -0600

    saving updated dominant variables and vis before data analysis

--- stream_visualization.m
+++ stream_visualization.m
@@ -3,8 +3,8 @@ exp_id = 12;
 obj_num = 24;
 
 subexpIDs = find_subjects({'cont_vision_size_obj9_child'},[exp_id]);
-vars = {'cstream_inhand_left-hand_obj-all_child','cstream_inhand_right-hand_obj-all_child','cstream_vision_size_obj-largest-child-inhand','cstream_vision_size_obj-largest-dominant_child', 'cstream_eye-vision_largest-roi_child', 'cstream_eye_roi_child','cstream_vision_size_obj-largest-parent-inhand','cstream_inhand_right-hand_obj-all_parent','cstream_inhand_left-hand_obj-all_parent'};
-labels = {'c_left', 'c_right','c_large','dominant','roi_large','c_roi','p_large','p_right','p_left'};
+vars = {'cstream_inhand_left-hand_obj-all_child','cstream_inhand_right-hand_obj-all_child','cstream_vision_size_obj-largest-dominant-child-inhand','cstream_vision_size_obj-largest-dominant_child', 'cstream_eye-vision_largest-dominant-roi_child', 'cstream_eye_roi_child','cstream_vision_size_obj-largest-dominant-parent-inhand','cstream_inhand_right-hand_obj-all_parent','cstream_inhand_left-hand_obj-all_parent'};
+labels = {'c_left', 'c_right','c_ha_dom','dominant','roi_dom','c_roi','p_ha_dom','p_right','p_left'};
 args.window_times_variable = [0 150;150 300;300 450;450 600];
 directory = '.';
 % note that directory = '.' will save in the current directory
